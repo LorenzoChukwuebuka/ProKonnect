4f06867552cd9f4a13d81aad1b0ef76da610052b
LorenzoChukwuebuka <Lawrenceobi2@gmail.com>
2023-01-18 10:48:38 +0100

    done

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -47,7 +47,7 @@ class GroupController extends Controller
             $guides = User::where('user_type', 'student')
                 ->join('user_interests', 'users.id', '=', 'user_interests.user_id')
                 ->whereIn('user_interests.interest_id', $user->userinterests()->pluck('interest_id'))
-                ->select('users.id', 'users.full_name', 'users.username', 'users.profile_image', 'users.status', 'users.country_id', 'users.bio')
+                ->select('users.id', 'users.full_name', 'users.username', 'users.email', 'users.bio', 'users.profile_image', 'users.status', 'users.country_id', 'users.phone_number')
                 ->when($request->search_user, function ($query) use ($request) {
                     $query->where("users.username", "like", "%" . $request->search_user . "%");
                 })
