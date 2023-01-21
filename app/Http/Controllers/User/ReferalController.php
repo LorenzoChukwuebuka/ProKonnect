b398418044846abf8d513c3f8db52e2315ca1eaf
LorenzoChukwuebuka <Lawrenceobi2@gmail.com>
2023-01-22 00:28:36 +0100

    done

--- app/Http/Controllers/User/ReferalController.php
+++ app/Http/Controllers/User/ReferalController.php
@@ -10,14 +10,27 @@ class ReferalController extends Controller
     public function get_referals_for_a_user()
     {
         try {
-            $referals = Referal::where('referal_id', auth()->user()->id)->first();
+            $referals = Referal::where('referal_id', auth()->user()->id)->lates()->get();
 
-            if ($referals == null) {
+            if ($referals->count() == 0) {
                 return response(["code" => 3, "message" => "No record found"]);
             }
 
             return response(["code" => 1, "data" => $referals]);
 
+        } catch (\Throwable$th) {
+            return response(["code" => 3, "error" => $th->getMessage()]);
+        }
+    }
+
+    public function get_referal_commission()
+    {
+        try {
+            $referal_transactions = ReferalTransaction::where('reffered_by', auth()->user()->id)->latest()->get();
+            if ($referal_transactions->count() == 0) {
+                return response(["code" => 3, "message" => "No record found"]);
+            }
+            return response(["code" => 1, "data" => $referal_transactions]);
         } catch (\Throwable$th) {
             return response(["code" => 3, "error" => $th->getMessage()]);
         }
